aad29d4681a0f88a715a13fc52fa36099a7445a0
Nilson Junior <nilson.silva@softvision.com.br>
2023-03-29 13:19:59 -0300

    Criando lista de cursos, componente table material

--- crud-angular/crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+
+  displayedColumns: string[] = ['name', 'category'];
+
+  courses: Course[] = [
+    { id: "1", name: "Angular", category: "Programação Front End" }
+  ];
 
   constructor() { }
 
