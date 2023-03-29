786a0b81684ce5828e6c1b3bf634fa1d4f2b7487
Nilson Junior <nilson.silva@softvision.com.br>
2023-03-28 21:01:59 -0300

    criando componente de contador

---
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'contador',
+  templateUrl: './output-property.component.html',
+  styleUrls: ['./output-property.component.css']
+})
+export class OutputPropertyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
