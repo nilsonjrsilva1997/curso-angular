e3f4632b7bfd2ab9a193d0b076b0ade99d2fd8bd
Nilson Junior <nilson.silva@softvision.com.br>
2023-03-29 11:51:46 -0300

    Acesso ao DOM e ao template com ViewChild

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -10,18 +10,20 @@ export class OutputPropertyComponent implements OnInit {
   @Input() valor:number = 0;
   @Output() mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput') campoValorInput!: ElementRef; 
+
   incrementa() {
-    this.valor++;
+    this.campoValorInput.nativeElement.value++;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
   decrementa() {
-    this.valor--;
+    this.campoValorInput.nativeElement.value--;
     this.mudouValor.emit({novoValor: this.valor});
   }
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void { }
-  
+
 }
